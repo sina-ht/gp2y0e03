f0c3962351d94fbdca6dfd06227f84e6fb666392
Hiroshi Takekawa <sian@big.or.jp>
2014-10-10 05:33:01 +0900

    Add licensing term

--- gp2y0e03.c
+++ gp2y0e03.c
@@ -1,5 +1,23 @@
 /* -*- c-basic-offset: 8 -*-
  * gp2y0e03.c -- GP2Y0E03 Distance Measuring Sensor Driver
+ * Copyright (C) 2014 Hiroshi Takekawa
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ATTENTION: GPL version 2 only. You cannot apply any later version.
+ * This situation may change in the future.
  */
 
 #include <stdio.h>
